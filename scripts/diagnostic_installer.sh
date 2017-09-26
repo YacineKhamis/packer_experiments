710a9e42638302bf79b5682f125a804d1c18f431
BenjaminJoly <benjamin.joly@securitymadein.lu>
2017-09-26 15:56:38 +0200

    Add some variables and minor modifs

--- scripts/diagnostic_installer.sh
+++ scripts/diagnostic_installer.sh
@@ -1,16 +1,19 @@
 #! /usr/bin/env bash
 
-PATH_TO_DIAGNOSTIC='/var/www/diagnostic'
-GITHUB_LINK='https://github.com/CASES-LU/diagnostic.git'
+PATH_TO_DIAGNOSTIC='/var/www/diagnostic'					#The path of the diagnostic in your VM
+GITHUB_LINK='https://github.com/CASES-LU/diagnostic.git'	#The Github path where you can find the diagnostic
 
-# Variables
-HOSTNAME='diagnostic'
-DB_NAME='diagnostic'
-DB_HOST='localhost'
-DBUSER_DIAGNOSTIC='diagnostic'
-DBPASSWORD_DIAGNOSTIC="$(openssl rand -hex 32)"
-DBUSER_ADMIN='root'
-DBPASSWORD_ADMIN="$(openssl rand -hex 32)"
+# Variables							
+DB_NAME='diagnostic' 								#The name of the Database
+DB_HOST='localhost'									#The IP Address where is located 
+DBUSER_DIAGNOSTIC='diagnostic'						#The DB user that will be used for the diagnostic
+DBPASSWORD_DIAGNOSTIC="$(openssl rand -hex 32)"		#The password of the user diagnostic of the DB; Random by default
+DBUSER_ADMIN='root'									#The administrator login of the DB
+DBPASSWORD_ADMIN="$(openssl rand -hex 32)"			#The password of the administrator of the DB; Random by default
+DEFAULT_LANGUAGE='en_EN'							#The default and main language of the diagnostic 
+IP_ADDRESS='10.0.0.102'								#The IP address where you will find the diagnostic
+DISABLE_MXCHECK=true								#If the VM is connected on internet (which is depreciate), it could check the validity of the mail used. If it set to false, no check are done.
+
 
 echo "\033[93m###############################################################################\\033[0m"
 echo "\033[93m#                             Diagnostic installer                            #\\033[0m"
@@ -41,15 +44,17 @@ if [ $? -ne 0 ]; then
     echo "ERROR: unable to clone the Diagnostic repository"
     exit 1;
 fi
-echo "\033[93mDisable MxCheck\\033[0m"
-sudo sed -i "/'useMxCheck' => true,/c\\\t\t\t'useMxCheck' => false," $PATH_TO_DIAGNOSTIC/module/Diagnostic/src/Diagnostic/InputFilter/LoginFormFilter.php
-sudo sed -i "/'useMxCheck' => true/c\\\t\t\t'useMxCheck' => false" $PATH_TO_DIAGNOSTIC/module/Admin/src/Admin/InputFilter/UserFormFilter.php
-sudo sed -i "/'useMxCheck' => true/c\\\t\t\t'useMxCheck' => false" $PATH_TO_DIAGNOSTIC/module/Admin/src/Admin/InputFilter/EmailNotExistFilter.php
-echo "\033[32mMxCheck disabled\\033[0m"
+if [ DISABLE_MXCHECK ]
+then
+	echo "\033[93mDisable MxCheck\\033[0m"
+	sudo sed -i "/'useMxCheck' => true,/c\\\t\t\t'useMxCheck' => false," $PATH_TO_DIAGNOSTIC/module/Diagnostic/src/Diagnostic/InputFilter/LoginFormFilter.php
+	sudo sed -i "/'useMxCheck' => true/c\\\t\t\t'useMxCheck' => false" $PATH_TO_DIAGNOSTIC/module/Admin/src/Admin/InputFilter/UserFormFilter.php
+	sudo sed -i "/'useMxCheck' => true/c\\\t\t\t'useMxCheck' => false" $PATH_TO_DIAGNOSTIC/module/Admin/src/Admin/InputFilter/EmailNotExistFilter.php
+	echo "\033[32mMxCheck disabled\\033[0m"
+fi
 echo "\033[32mdiagnostic sources copied\\033[0m"
 
 echo "\033[93mcomposer installation\\033[0m"
-# TODO
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -59,13 +64,8 @@ fi
 composer self-update
 composer install -o --prefer-dist
 echo "\033[32mcomposer installation done\\033[0m"
-#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#php composer-setup.php
-#php -r "unlink('composer-setup.php');"
-#php composer.phar install
-# sql_init script should be put in scripts/ repository, which is not the case now
+
 echo "\033[93mmysql installation\\033[0m"
-# TODO
 echo "mysql-server mysql-server/root_password password $DBPASSWORD_ADMIN" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DBPASSWORD_ADMIN" | sudo debconf-set-selections
 sudo apt-get -qq install mysql-server > /dev/null 2>&1
@@ -81,16 +81,6 @@ mysql -u root -p"$DBPASSWORD_ADMIN" -e "source ./scripts/db_initialization.sql" 
 mysql -u root -p"$DBPASSWORD_ADMIN" -e "CREATE USER 'diagnostic'@'localhost' IDENTIFIED BY '$DBPASSWORD_DIAGNOSTIC'" > /dev/null 2>&1
 mysql -u root -p"$DBPASSWORD_ADMIN" -e "GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE on diagnostic.* to 'diagnostic'@'localhost'" > /dev/null 2>&1
 echo "\033[32mdatabase ready\\033[0m"
-
-
-
-#declare -A global_configurationArray
-#global_configurationArray["%%DB_NAME%%"]=$DB_NAME
-#global_configurationArray["%%DB_HOST%%"]=$DB_HOST
-
-#declare -A local_configurationArray
-#local_configurationArray["%%DB_USER%%"]=$DBUSER_DIAGNOSTIC
-#local_configurationArray["%%DB_PASSWORD%%"]=$DBPASSWORD_DIAGNOSTIC
 
 cp $PATH_TO_DIAGNOSTIC/config/autoload/global.php.dist $PATH_TO_DIAGNOSTIC/config/autoload/global.php
 cat > $PATH_TO_DIAGNOSTIC/config/autoload/global.php <<EOF
@@ -111,17 +101,6 @@ return [
 ];
 EOF
 
-#global_configure() {
-    # Loop the global config array
-#    for i in "${!global_configurationArray[@]}"
-#    do
-#        search=$i
-#        replace=${global_configurationArray[$i]}
-#        sudo sed -i "s/${search}/${replace}/g" $PATH_TO_DIAGNOSTIC/config/autoload/global.php
-#    done
-#}
-#global_configure
-
 cp $PATH_TO_DIAGNOSTIC/config/autoload/local.php.dist $PATH_TO_DIAGNOSTIC/config/autoload/local.php
 cat > $PATH_TO_DIAGNOSTIC/config/autoload/local.php <<EOF
 <?php
@@ -136,19 +115,6 @@ return [
     'domain' => 'diagnostic.cases.lu',
 ];
 EOF
-
-
-#local_configure() {
-    # Loop the local config array
-#    for i in "${!local_configurationArray[@]}"
-#    do
-#        search=$i
-#        replace=${local_configurationArray[$i]}
-#        sudo sed -i "s/${search}/${replace}/g" $PATH_TO_DIAGNOSTIC/config/autoload/local.php
-#    done
-#}
-#local_configure
-
 
 echo "\033[93mvirtual host configuration\\033[0m"
 sudo cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
@@ -170,7 +136,7 @@ sudo service apache2 restart > /dev/null 2>&1
 echo "\033[32mvirtual host configuration done\\033[0m"
 
 key="%%LANG%%"
-replace="en_EN"
+replace=$DEFAULT_LANGUAGE
 sudo sed -i "s/${key}/${replace}/g" $PATH_TO_DIAGNOSTIC/module/Diagnostic/config/module.config.php
 
 sudo chown -R www-data $PATH_TO_DIAGNOSTIC
@@ -190,7 +156,7 @@ iface lo inet loopback
 # The primary network interface
 auto enp0s8
 iface enp0s8 inet static
-	address 10.0.0.102
+	address $IP_ADDRESS
 	netmask 255.0.0.0
 	gateway 10.0.0.1
 EOF
@@ -200,7 +166,7 @@ echo "\033[32mnetworkconfiguration done\\033[0m"
 echo "\033[93m###############################################################################\\033[0m"
 echo "\033[93m#                                  FINISHED                                   #\\033[0m"
 echo "\033[93m#            You can now access the application by typing in                  #\\033[0m"
-echo "\033[93m#            \033[33mhttp://10.0.0.102                                                \033[93m#\\033[0m"
+echo "\033[93m#            \033[33mhttp://$IP_ADDRESS                                             \033[93m#\\033[0m"
 echo "\033[93m#                        in your favorite browser.                            #\\033[0m"
 echo "\033[93m#                   \033[92mLogin : diagnostic@cases.lu                               \033[93m#\\033[0m"
 echo "\033[93m#                   \033[92mPassword : Diagnostic1!                                   \033[93m#\\033[0m"
